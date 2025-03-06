fe68c72be89abc7efbce2ddf0af30a98d2a126ef
OlyaJean <olga_lavida@yahoo.com>
2025-03-06 00:47:43 -0500

    calender

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import './style.css'
 
 
+const toDay = document.querySelector('.toDay');
+const toDate = document.querySelector('.toDate')
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+const today = new Date();
+
+window.addEventListener('DOMContentLoaded',()=>{
+  toDay.textContent = days[today.getDay()]
+  const month = today.toLocaleString('default',{month:'long'})
+  toDate.textContent = `${month} ${today.getDate()}, ${today.getFullYear()}`
+})
